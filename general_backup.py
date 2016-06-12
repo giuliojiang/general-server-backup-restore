c9264ff8e2adccda4c17094de80b408ecc7e4058
Giulio Jiang <giuliojiang@gmail.com>
2016-06-12 14:19:13 +0100

    fixing the script

--- general_backup.py
+++ general_backup.py
@@ -1,66 +1,83 @@
 import subprocess
 import sys
-import colors
 import random
 import os
 import argparse
+import shutil
 
 # Output files:
 # output_path/database.sql.tgz
 # output_path/archive_###.tgz
 
 def backup_sql(output_path):
-	# create SQL dump
-	dumpfile = output_path + os.sep + 'database.sql'
-	cmd = 'mysqldump -u root -p --all-databases > {}'.format(output_path)
-	code = subprocess.call(cmd, shell=True)
-	if code != 0:
-		return code
-		
-	# compress the dump
-	dump_compressed_file = output_path + os.sep + 'database.sql.tgz'
-	cmd = []
-	cmd.append('tar')
-	cmd.append('-avcf')
-	cmd.append(dump_compressed_file)
-	cmd.append(dumpfile)
-	code = subprocess.call(cmd)
-	
-	return code
-	
-	
+    # create SQL dump
+    dumpfile = output_path + os.sep + 'database.sql'
+    cmd = 'mysqldump -u root -p --all-databases > {}'.format(output_path)
+    code = subprocess.call(cmd, shell=True)
+    if code != 0:
+        return code
+        
+    # compress the dump
+    dump_compressed_file = output_path + os.sep + 'database.sql.tgz'
+    cmd = []
+    cmd.append('tar')
+    cmd.append('-avcf')
+    cmd.append(dump_compressed_file)
+    cmd.append(dumpfile)
+    code = subprocess.call(cmd)
+    
+    return code
+    
+def remove_newlines(s):
+    out = ''
+    i = 0
+    
+    # Skip initial initial spaces and tabs
+    while s[i] == ' ' or s[i] == '\t':
+        i += 1
+    
+    # Retrieve until newline
+    while i < len(s) and s[i] != '\n':
+        out += s[i]
+        i += 1
+        
+    return out
+    
+    
 def find_abs_path_relative_of(query_path, relative_path):
-	previous_working_dir = os.path.abspath(os.getcwd())
-	os.chdir(relative_path)
-	result = os.path.abspath(query_path)
-	os.chdir(previous_working_dir)
-	return result
-	
-	
+    previous_working_dir = os.path.abspath(os.getcwd())
+    os.chdir(relative_path)
+    result = os.path.abspath(query_path)
+    os.chdir(previous_working_dir)
+    return remove_newlines(result)
+    
+    
 def backup_directories(input_file_path, output_path, original_working_directory):
-	os.chdir('/')
-	try:
-		input_file = open(input_file_path, 'r')
-	except:
-		print('Error opening file {}'.format(input_file_path))
-		return -1
-	
-	counter = 0
-	for line in input_file:
-		input_folder_path = find_abs_path_relative_of(line, original_working_directory)
-		output_archive_name = output_path + os.sep + 'archive_{}.tgz'.format(counter)
-		cmd = []
-		cmd.append('tar')
-		cmd.append('-avcf')
-		cmd.append(output_archive_name)
-		cmd.append(input_folder_path)
-		code = subprocess.call(cmd)
-		if code != 0:
-			print('Error when compressing folder {}'.format(line))
-			return code
-			
-	return 0
-	
+    os.chdir('/')
+    try:
+        input_file = open(input_file_path, 'r')
+    except:
+        print('Error opening file {}'.format(input_file_path))
+        return -1
+    
+    counter = 0
+    for line in input_file:
+        input_folder_path = find_abs_path_relative_of(line, original_working_directory)
+        if len(input_folder_path) == 0:
+            continue
+        output_archive_name = output_path + os.sep + 'archive_{}.tgz'.format(counter)
+        cmd = []
+        cmd.append('tar')
+        cmd.append('-avcf')
+        cmd.append(output_archive_name)
+        cmd.append(input_folder_path)
+        code = subprocess.call(cmd)
+        if code != 0:
+            print('Error when compressing folder {}'.format(line))
+            return code
+            
+    return 0
+    
 
 def main(argv=None):
     # ..........................................................................
@@ -71,12 +88,10 @@ def main(argv=None):
     # ..........................................................................
     # parse command line arguments (argparse)
     parser = argparse.ArgumentParser()
-	
-    parser.add_argument('input',
+    
+    parser.add_argument('folders',
                         help='List of directories to save',
-                        nargs=1,
-                        default=None,
-						required=True
+                        default=None
                         )
 
     parser.add_argument('--out',
@@ -91,24 +106,34 @@ def main(argv=None):
                         )
 
     args = parser.parse_args(argv)
-	
-	code = 0
+    
+    code = 0
 
-	input_file_path = os.path.abspath(args.input)
-	output_path = os.path.abspath(args.out)
-	current_working_directory = os.path.abspath(os.getcwd())
+    input_file_path = os.path.abspath(args.folders)
+    output_path = os.path.abspath(args.out)
+    current_working_directory = os.path.abspath(os.getcwd())
+    
+    # setup output directory
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    else:
+        print('Output directory already exists. Press ENTER to delete its content and proceed, or Ctrl+C to cancel')
+        raw_input('')
+        shutil.rmtree(output_path)
+        os.makedirs(output_path)
 
-	
-	if args.sql:
-		code = backup_sql(output_path)
-	if code != 0:
-		print('MySQL backup returned {}'.format(code))
-		return code
-	
-	code = backup_directories(input_file_path, output_path, current_working_directory)
-	if code != 0:
-		print('Directories backup not completed successfully')
-		return code
+    # backup MySQL
+    if args.sql:
+        code = backup_sql(output_path)
+    if code != 0:
+        print('MySQL backup returned {}'.format(code))
+        return code
+    
+    # backup folders
+    code = backup_directories(input_file_path, output_path, current_working_directory)
+    if code != 0:
+        print('Directories backup not completed successfully')
+        return code
 
     return code
     
